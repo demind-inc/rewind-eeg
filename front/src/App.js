88f305ac5f66bc233d87c5932c4c067b379336c0
naomike <yukawanao@gmail.com>
2023-12-02 13:35:31 -0500

    add: read CSV file button

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactFileReader from "react-file-reader";
+import Papa from "papaparse";
+
+import "./App.css";
 
 function App() {
+  const uploadFile = (files) => {
+    Papa.parse(files[0], {
+      complete: function (results) {
+        const eegData = results.data.slice(2);
+        const attentionScores = eegData.map(
+          (row) => row[results.data[1].indexOf("PM.Attention.Scaled")]
+        );
+        const timestamps = eegData.map(
+          (row) => row[results.data[1].indexOf("Timestamp")]
+        );
+
+        const nonEmptyAttentionIndices = [];
+        for (let i = 0; i < attentionScores.length; i++) {
+          if (attentionScores[i] !== "") {
+            nonEmptyAttentionIndices.push(i);
+          }
+        }
+        const attentionScoresOfNonEmpty = nonEmptyAttentionIndices.map(
+          (index) => attentionScores[index]
+        );
+        const timestampsOfNonEmpty = nonEmptyAttentionIndices.map(
+          (index) => timestamps[index]
+        );
+      },
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Rewind EEG</p>
+        <ReactFileReader handleFiles={uploadFile} fileTypes={".csv"}>
+          <button className="btn"> Upload EEG Data</button>
+        </ReactFileReader>
         <a
           className="App-link"
           href="https://reactjs.org"
