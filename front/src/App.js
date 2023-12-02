eabd65c6558e5bfad266582cdde2c9926f44c344
naomike <yukawanao@gmail.com>
2023-12-02 15:59:56 -0500

    add: eeg chart

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactFileReader from "react-file-reader";
 import Papa from "papaparse";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
 import "./App.css";
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 function App() {
+  const [chartData, setChartData] = useState({});
+
   const uploadFile = (files) => {
     Papa.parse(files[0], {
       complete: function (results) {
@@ -18,36 +41,67 @@ function App() {
 
         const nonEmptyAttentionIndices = [];
         for (let i = 0; i < attentionScores.length; i++) {
-          if (attentionScores[i] !== "") {
+          if (attentionScores[i] && attentionScores[i] !== "") {
             nonEmptyAttentionIndices.push(i);
           }
         }
         const attentionScoresOfNonEmpty = nonEmptyAttentionIndices.map(
-          (index) => attentionScores[index]
+          (index) => Number(attentionScores[index])
         );
-        const timestampsOfNonEmpty = nonEmptyAttentionIndices.map(
-          (index) => timestamps[index]
+        const timestampsOfNonEmpty = nonEmptyAttentionIndices.map((index) =>
+          parseInt(Number(timestamps[index]))
         );
+
+        setChartData({
+          labels: timestampsOfNonEmpty,
+          datasets: [
+            {
+              label: "Attention Score",
+              data: attentionScoresOfNonEmpty,
+              fill: false,
+              borderColor: "rgba(75,192,192,1)",
+            },
+          ],
+        });
       },
     });
   };
 
+  const options = {
+    scales: {
+      x: {
+        type: "linear",
+        position: "bottom",
+        title: {
+          display: true,
+          text: "Timestamp",
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: "Attention Score",
+        },
+      },
+    },
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Rewind EEG</p>
-        <ReactFileReader handleFiles={uploadFile} fileTypes={".csv"}>
-          <button className="btn"> Upload EEG Data</button>
-        </ReactFileReader>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Rewind EEG</p>
+      <ReactFileReader handleFiles={uploadFile} fileTypes={".csv"}>
+        <button className="btn"> Upload EEG Data</button>
+      </ReactFileReader>
+      <div className="ChartArea">
+        {Object.keys(chartData).length ? (
+          <>
+            <Line data={chartData} options={options} />
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+      <div className="ScreenShotArea"></div>
     </div>
   );
 }
